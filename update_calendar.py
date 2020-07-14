b06f91fc7b7c08b193c414af12a169635e3960c0
Fab <fabianjarrett@gmail.com>
2020-07-14 10:34:20 +0100

    Make scope a list so it works

--- update_calendar.py
+++ update_calendar.py
@@ -8,7 +8,7 @@ from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 
-SCOPES = 'https://www.googleapis.com/auth/calendar.events'
+SCOPES = ['https://www.googleapis.com/auth/calendar.events']
 START_YEAR_TEMPLATE = "{}-01-01T00:00:00Z"
 
 
